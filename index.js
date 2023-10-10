311c4776bf0628bfd209f819a9e00c662d47a31a
Asif Shaikh <professional.dev9717@gmail.com>
2023-10-10 16:31:56 +0530

    ArgoCD implementation in minikube

---
+++ index.js
@@ -0,0 +1,11 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World from Kubernetes!');
+});
+
+server.listen(8080, function() {
+  console.log('Server running on port 8080');
+});
